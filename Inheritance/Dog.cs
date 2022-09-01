f412022a86cbe1f860577de8f81c28017b9f0d7b
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-09-01 06:39:55 +0100

    Add project files.

---
+++ Inheritance/Dog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Dog : Animal
+    {
+        public bool IsHappy { get; set; }//prop only for the dog
+
+        //constructor
+        public Dog(string name, int age) : base(name, age)
+        {
+            IsHappy = true;
+        }
+
+        public override void Eat()
+        {
+            base.Eat();//keeping the functionality of the base method
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Wuuuf!"); //overwriting the method
+        }
+
+        public override void Play()
+        {
+            if (IsHappy)
+            {
+                base.Play();
+            }
+            else
+            {
+                Console.WriteLine($"{Name} doesn't want to play");
+            }
+
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog("Oreo", 1);
+            Console.WriteLine($"{dog.Name} is {dog.Age} year old.");
+            dog.MakeSound();
+            dog.Play();
+            dog.Eat();
+        }
+    }
+}
